a913895cd2c2da4cd005eb8293f3b840a298b8d2
seuoh22 <70955060+ohhseungyeon@users.noreply.github.com>
2022-06-17 01:21:57 +0900

    feat: switch images as progressValues change

--- ggwadang/MainView.swift
+++ ggwadang/MainView.swift
@@ -12,7 +12,7 @@ var today = Date()
 struct MainView: View {
     @State private var todaySugarValue: Int = 18
     @State private var sugarGoalValue: Int = 40
-    @State var progressValue: Float = 0.63
+    @State var progressValue: Float = 0.98
     
     static var dateFormat: DateFormatter {
         let formatter = DateFormatter()
@@ -22,29 +22,27 @@ struct MainView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                ZStack {
-                    Text("\(today, formatter: MainView.dateFormat)")
-                        .foregroundColor(.white)
-                        .frame(maxWidth:.infinity, alignment: .center)
-                    NavigationLink(destination: SettingView()) {
-                        Image(systemName: "gearshape")
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .frame(maxWidth: .infinity, alignment: .trailing)
+                VStack {
+                    ZStack {
+                        Text("\(today, formatter: MainView.dateFormat)")
                             .foregroundColor(.white)
-                            .padding(20.0)
+                            .frame(maxWidth:.infinity, alignment: .center)
+                        NavigationLink(destination: SettingView()) {
+                            Image(systemName: "gearshape")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .foregroundColor(.white)
+                                .padding(20.0)
+                        }
                     }
-                }
-                ZStack {
                     ProgressCircleView(progress: self.$progressValue)
-                                }
-                                Spacer()
-                            }.background(LinearGradient(gradient: Gradient(colors: [Color("mint"), Color("green")]), startPoint: .top, endPoint: .bottom))
+                    Spacer()
+                }.background(LinearGradient(gradient: Gradient(colors: [Color("mint"), Color("green")]), startPoint: .top, endPoint: .bottom))
         }
-                .navigationBarTitle("main")
-            }
-        }
+        .navigationBarTitle("main")
+    }
+}
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
--- ggwadang/ProgressCircleView .swift
+++ ggwadang/ProgressCircleView .swift
@@ -9,26 +9,51 @@ import SwiftUI
 
 struct ProgressCircleView : View {
     @Binding var progress: Float
-    
+
     var body: some View {
         ZStack {
             ZStack{
-            Circle()
-                .stroke(lineWidth: 23.0)
-                .opacity(0.25)
-                .foregroundColor(Color.white)
-            
-            Circle()
-                .trim(from:0.0, to: CGFloat(min(self.progress, 1.0)))
-                .stroke(style: StrokeStyle(lineWidth: 23.0, lineCap: .round, lineJoin: .round))
-                .foregroundColor(.white)
-                .rotationEffect(Angle(degrees: 270.0))
-                .animation(.linear)
+                Circle()
+                    .stroke(lineWidth: 23.0)
+                    .opacity(0.25)
+                    .foregroundColor(Color.white)
+                
+                Circle()
+                    .trim(from:0.0, to: CGFloat(min(self.progress, 2.0)))
+                    .stroke(style: StrokeStyle(lineWidth: 23.0, lineCap: .round, lineJoin: .round))
+                    .foregroundColor(.white)
+                    .rotationEffect(.degrees(-90.0))
+                    .animation(.easeOut, value: progress)
             }.frame(width: 250, height: 250)
-        }//.background(.yellow)
+            
+            ZStack {
+                Text(String(format: "%.0f %%", min(self.progress, 1.0)*100.0))
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.white)
+                
+                if progress <= 0.39 {
+                    Image("꽈당이 1")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                } else if progress <= 0.79 {
+                    Image("꽈당이 2")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                } else if progress <= 0.99 {
+                    Image("꽈당이 3")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                } else {
+                    Image("꽈당이 4")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+
+            }//.background(.yellow)
+        }.padding()
     }
 }
-
+}
 struct ProgressCircleView_Previews: PreviewProvider {
     static var previews: some View {
         ProgressCircleView(progress: .constant(30))
